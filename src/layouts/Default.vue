1021b49b2cc02d27818011db3c6102366c19343a
Mateusz <kawkamateusz@pm.me>
2020-11-18 22:00:18 +0100

    init

---
+++ src/layouts/Default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="layout">
+    <header class="header">
+        <h1><g-link to="/">{{ $static.metadata.siteName }}</g-link></h1>
+    </header>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+.layout {
+  max-width: 760px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+</style>
