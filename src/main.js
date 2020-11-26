3f14626b8338a16a40b2e614b86a3adb20461e19
Mateusz <kawkamateusz@pm.me>
2020-11-26 11:21:33 +0100

    add local fonts

--- src/main.js
+++ src/main.js
@@ -2,19 +2,9 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from "~/layouts/Default.vue";
+import "./assets/fonts.css"
 import "./assets/markdown-body.css";
 
 export default function(Vue, { router, head, isClient }) {
-  head.link.push({
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-  });
-
-  head.link.push({
-    rel: "stylesheet",
-    href:
-      "https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap",
-  });
-
   Vue.component("Layout", DefaultLayout);
 }
