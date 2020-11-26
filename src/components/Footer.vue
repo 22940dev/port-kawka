3d5248e619db3c94600b3763fc14e509d3491671
Mateusz <kawkamateusz@pm.me>
2020-11-26 10:53:41 +0100

    move footer to component

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="footer">
+    <a class="back-to-top-link" href="" @click="backToTop">Back to top</a>
+    <p>
+      Build with
+      <a
+        class="footer__link"
+        href="https://gridsome.org/"
+        rel="noopener noreferrer"
+        target="_blank"
+        >Gridsome</a
+      >
+      & made with ❤️ by Kawka Mateusz
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  methods: {
+    backToTop() {
+      window.scrollTo(0, 0);
+    },
+  }
+};
+</script>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -4,36 +4,20 @@
     <main>
       <slot />
     </main>
-    <footer class="footer">
-      <a class="back-to-top-link" href="" @click="backToTop">Back to top</a>
-      <p>
-        Build with
-        <a
-          class="footer__link"
-          href="https://gridsome.org/"
-          rel="noopener noreferrer"
-          target="_blank"
-          >Gridsome</a
-        >
-        & made with ❤️ by Kawka Mateusz
-      </p>
-    </footer>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default {
   name: "DefaultLayout",
   components: {
     Header,
-  },
-  methods: {
-    backToTop() {
-      window.scrollTo(0, 0);
-    },
-  },
+    Footer
+  }
 };
 </script>
 
